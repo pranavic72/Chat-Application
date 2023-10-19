10fefe2d48b458f5780dda3ff1ac69d2aa06dacc
pranavic72 <pranavic72@gmail.com>
2023-10-19 21:27:56 +0530

    first commit

---
+++ run.py
@@ -0,0 +1,38 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template, request, session, url_for
+
+app = Flask (__name__)
+app.secret_key = os.getenv("SECRET", "randomstring123")  
+
+messages = []
+
+def add_message(username, message):
+   
+    now = datetime.now().strftime("%H:%M:%S")
+    
+    messages.append({"from": username, "message":message})
+
+@app.route('/', methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        session["username"] = request.form["username"]
+        
+    if "username" in session:
+        return redirect(url_for("user", username=session["username"]))
+    
+    return render_template("index.html")
+
+@app.route('/chat/<username>', methods = ["GET", "POST"])
+def user(username):
+    if request.method == "POST":
+        username = session["username"]
+        message = request.form["message"]
+        add_message(username,message)
+        return redirect(url_for("user", username=session["username"]))
+    
+    return render_template("chat.html", username = username, chat_messages = messages)
+    
+app.run(host=os.getenv('IP', "0.0.0.0"), port=int(os.getenv('PORT', "5000")), debug=False) # debug=False for production. 
+
+
